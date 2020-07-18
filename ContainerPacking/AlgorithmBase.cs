3007fa87737eb56bfe29ecd4ab8c057fe982e9fd
q <q@>
2020-07-18 15:03:43 +0300

    initial

---
+++ ContainerPacking/AlgorithmBase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace ContainerPacking
+{
+	public abstract class AlgorithmBase
+	{
+		public abstract ContainerPackingResult Run(Container container, List<Item> items);
+	}
+}
---
+++ test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using ContainerPacking;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var containers = new List<Container>();
+            containers.Add(new Container(1, 100, 200, 300));
+
+            var itemsToPack = new List<Item>();
+            itemsToPack.Add(new Item(1, 10, 20, 30, 500));
+
+            var algorithms = new List<int>();
+            algorithms.Add((int)AlgorithmType.EB_AFIT);
+
+            var result = PackingService.Pack(containers, itemsToPack, algorithms);
+        }
+    }
+}
